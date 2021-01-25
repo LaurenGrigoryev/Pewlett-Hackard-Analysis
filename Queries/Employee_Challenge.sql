8c4aec636dbb64d6f3235400b349b3e85450d7fc
LaurenSonis <75095346+LaurenSonis@users.noreply.github.com>
2021-01-24 21:34:25 -0500

    Employees_Challenge.sql

---
+++ Queries/Employee_Challenge.sql
@@ -0,0 +1,100 @@
+-- Creating tables for Employee_DB
+CREATE TABLE Employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+	 first_name VARCHAR NOT NULL,
+	 last_name VARCHAR NOT NULL,
+	 gender VARCHAR NOT NULL,
+	 hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+     );
+CREATE TABLE Departments (
+     dept_no VARCHAR NOT NULL,
+	 dept_name VARCHAR NOT NULL,
+     PRIMARY KEY (dept_no),
+	 UNIQUE (dept_name)
+     );
+CREATE TABLE Dept_Emp (
+     emp_no INT NOT NULL,
+     dept_no VARCHAR NOT NULL,
+	 from_date DATE NOT NULL,
+	 to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+     PRIMARY KEY (emp_no, dept_no)
+     );	 
+CREATE TABLE Dept_Manager (
+dept_no VARCHAR NOT NULL,     
+emp_no INT NOT NULL,
+	 from_date DATE NOT NULL,
+	 to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	 FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
+	 PRIMARY KEY (dept_no, emp_no, from_date, to_date)
+     );
+CREATE TABLE Salaries (
+     emp_no INT NOT NULL,
+     salary INT NOT NULL,
+	 from_date DATE NOT NULL,
+	 to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	 PRIMARY KEY (emp_no)
+     );
+CREATE TABLE Titles (
+     emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+	 from_date DATE NOT NULL,
+	 to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	 PRIMARY KEY (emp_no, title, from_date)
+     );
+-- Select into new table
+
+SELECT emp_no, first_name, last_name
+	INTO retirement_info
+	FROM employees;
+SELECT title, from_date, to_date
+	INTO retirement_info
+	FROM titles;
+-- JOIN tables
+	SELECT retirement_titles.emp_no,
+       retirement_titles.first_name,
+       retirement_titles.last_name,
+       titles.title,
+       titles.from_date,
+       titles.to_date
+	INTO retirement_titles_table
+	FROM retirement_titles
+	LEFT JOIN titles
+	ON retirement_titles.emp_no = titles.emp_no;
+	SELECT * FROM retirement_titles_table
+-- Filter on birth_date
+SELECT * FROM employees 
+	WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY employees.emp_no
+-- Use Distinct with Orderby to remove duplicate rows
+
+SELECT DISTINCT ON (retirement_titles_table.emp_no) 
+retirement_titles_table.emp_no, 
+retirement_titles_table.first_name,
+retirement_titles_table.last_name,
+retirement_titles_table.title
+INTO Unique_titles
+FROM retirement_titles_table
+ORDER BY retirement_titles_table.emp_no, retirement_titles_table.to_date DESC;
+-- get number of titles
+-- get number of titles
+SELECT COUNT (title)
+FROM Unique_titles;
+-- answer 300024
+SELECT DISTINCT ON (unique_titles.title, unique_titles.count) 
+	unique_titles.count,
+	unique_titles.title 
+INTO Retiring_titles
+	FROM Unique_titles
+	GROUP BY unique_titles.title
+	ORDER BY unique_titles.count DESC;
+SELECT * FROM retiring_titles
+
+
+
